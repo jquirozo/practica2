723bba14824a017640a4ee84a07b20b79bd4b485
Jacobo <try1985>
2012-10-27 11:27:54 -0500

    practica 2
    se sube por primera vez

---
+++ js/action.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+function deviceInfo(){
+	$('#device table td').eq(1).text(device.name);
+	$('#device table td').eq(3).text(device.phonegap);
+	$('#device table td').eq(5).text(device.platform);
+	$('#device table td').eq(7).text(device.version);
+	$('#device table td').eq(9).text(device.uuid);
+	}
+$(document).ready(function(e) {
+	deviceInfo();
+    document.addEventListener("deviceready",function(){
+		 deviceInfo();
+		},false);
+});
